ce0dbcb3b126f93096a45f83f2b103f24518e510
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-13 23:26:58 +0200

    app js

--- js/app2.js
+++ js/app2.js
@@ -7,9 +7,9 @@ console.log(parola)
 
 lastNameUpperCase= lastName.charAt(0).toUpperCase();
 lastNameLowerCase= lastName.slice(1).toLowerCase();
-lastNameString= lastNameUpperCase + lastNameLowerCase; */
+lastNameString= lastNameUpperCase + lastNameLowerCase;
 
 var list=[1,2,3,4,5,6];
 for(var i=0; i<list.length; i++){
     console.log(list[i])
-}
+} */
